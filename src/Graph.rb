f08b8a4ead7844af7a8259406c96305cbade33b2
Stefano <s.martinallo@gmail.com>
2014-11-01 15:37:37 +0100

    TEST: tests for Node, Graph
    * add tests for Node and Graph classes
    * getter and setter in ruby style
    * holding nil pointers in methods

--- src/Graph.rb
+++ src/Graph.rb
@@ -7,156 +7,54 @@
 #
 # The Node class is used to abstract the graph node concept.
 class Node
-    # Class constructor.
-    #
-    # ==== Attributes
-    #
-    # * +id+         The Node identifier
-    # * +neighbors+  The set of neighbors (key,value) pairs
-    # * +failure+    The failure function used by Aho-Corasick
-    # * +output+     The output function used by Aho-Corasick
-    # * +goto_value+ The set of neighbors we can reach using the edge id.
-    #
-    def initialize(id)
-        @id         = id
-        @neighbors  = {}
+
+    attr_reader   :name, :edges, :output
+    attr_accessor :failure
+
+    def initialize(name="")
+        @name       = name
+        @edges      = {}
         @failure    = 0
         @output     = []
-        @goto_value = {}
     end
 
-    # ==== Return
-    #
-    # The Node identifier.
-    #
-    def id
-        @id        
-    end
-
-    # ==== Return
-    #
-    # The neighbors of the given Node.
-    #
-    def neighbors
-        @neighbors
-    end
-
-    # ==== Return
-    #
-    # The value of the failure function.
-    #
-    def failure
-        @failure
-    end
-
-    # Set the value of the the failure function.
-    #
-    # ==== Attributes
-    #
-    # * +value+ The value we want to set.
-    #
-    def failure=(value)
-        @failure = value        
-    end
-
-    # ==== Return
-    #
-    # The value of the output function.
-    #
-    def output
-        @output
-    end
-
-    # Set the value of the the output function.
-    #
-    # ==== Attributes
-    #
-    # * +output+ The value we want to set.
-    #
-    def output=(output)
-        @output = output
-    end
-
-    # Add the keyword to the output of the give node.
-    #
-    # ==== Attributes
-    #
-    # * +keyword+  The keyword to add.
-    #
     def add_output(keyword)
-        @output << keyword
+        unless (not keyword) || (keyword.length==0)
+            @output << keyword
+        end
     end
 
-    # Get the value of the goto function.
-    #
-    # ==== Attributes
-    #
-    # * +edge_id+  The edge identifier.
-    #
-    # ==== Return
-    #
-    # * The identifier of the reachable Node using the given edge_id.
-    # * nil on failure
-    #
-    def goto(edge_id)
-        @goto_value[edge_id]
-    end
-
-    # Connect two nodes by using an edge.
-    # 
-    # ==== Attributes
-    #
-    # * +node_id+ The node we want connect to.
-    # * +edge_id+ The id of the created edge.
-    #
-    # ==== Usage
-    #
-    #    n1 = Node.new(1)
-    #    n2 = Node.new(2)
-    #    n1.connect_to(2,"edge 1-->2")
-    #
-    def connect_to(node_id, edge_id)
-        if ( not @neighbors.include?(node_id) )
-            @neighbors[node_id]  = edge_id
-            @goto_value[edge_id] = node_id 
+    def goto(edge)
+        unless (not edge) || (not edge)
+            @edges[edge]
         end
     end
 
     # ==== Attributes
     #
-    # * +edge_id+ The edge id we want to find.
+    # * +node_ptr+ Pointer to the node we want connect to.
+    # * +edge+     The tag of the created edge.
     #
-    # ==== Return
-    #
-    # True if the node has an edge with the give identifier, false otherwise.
-    #
-    def has_edge_with_id?(edge_id)
-        return (@neighbors.values.include?(edge_id))
+    def connect_to(node_ptr, edge)
+        unless (not node_ptr) ||
+               (not edge) || (edge.length==0 if edge.instance_of?(String)) ||
+               ( @edges[edge] == node_ptr )     # avoid two identical edges to the same node
+            @edges[edge] = node_ptr
+        end
     end
 
-    # ==== Attributes
-    #
-    # * +node_id+ The Node we want to find.
-    #
-    # ==== Return
-    #
-    # True if the node is connected to node_id, false otherwise.
-    #
-    def is_connected_to?(node_id)
-        return (@neighbors.keys.include?(node_id))
+    def has_edge?(edge)
+        return (@edges.include?(edge))
     end
+
 end
 
 
 # The Graph class is used to abstract the graph concept.
 class Graph
 
-    # Class constructor.
-    # Initialize a generic Graph.
-    # ==== Attributes
-    #
-    # * +nodes+     The set of the nodes.
-    #
+    attr_reader   :nodes
+
     def initialize
         @nodes = {}
     end
@@ -168,28 +66,18 @@ class Graph
     #
     # * +id+     The Node identifier.
     #
-    def add_node(id)
-        @nodes[id] = Node.new(id) if not @nodes.include?(id)
+    def add_node(with_name)
+        unless ( not with_name ) ||
+               ( with_name.length==0 if with_name.instance_of?(String) ) ||
+               ( @nodes.include?(with_name) )
+            @nodes[with_name] = Node.new(with_name)
+        end
     end
 
-    # ==== Return
-    #
-    # The list of nodes.
-    #
-    def nodes
-        @nodes.values
-    end
-
-    # ==== Attributes
-    #
-    # * +id+     The Node identifier.
-    #
-    # ==== Return
-    #
-    # The pointer to the identified node.
-    #
-    def node!(id)
-        @nodes[id]
+    def node!(name)
+        unless ( not name ) || ( name.length==0 if name.instance_of?(String) )
+            @nodes[name]
+        end
     end
 
     # Add an edge between two nodes.
@@ -197,19 +85,21 @@ class Graph
     #
     # ==== Attributes
     #
-    # * +node_id_from+  The node where the edge starts from.
-    # * +node_id_to+    The node where the edge ends to.
-    # * +edge_id+       The edge identifier.
+    # * +from+  The node where the edge starts from.
+    # * +to+    The node where the edge ends to.
+    # * +edge+  The edge name.
     #
     # ==== Return
     #
     # On success it creates the edge.
     #
-    def add_edge(node_id_from, node_id_to, edge_id)
-        if (not @nodes.include?(node_id_from)) || (not @nodes.include?(node_id_to))
-            puts "(#{node_id_from},#{node_id_to}) One of the nodes does not exist"
-        else
-            node!(node_id_from).connect_to(node_id_to, edge_id)
+    def add_edge(from, to, edge)
+        unless ( not from ) || ( from.length==0 if from.instance_of?(String) ) || 
+               ( not to   ) || (   to.length==0 if   to.instance_of?(String) ) ||
+               ( not edge ) || ( edge.length==0 if edge.instance_of?(String) ) ||
+               ( not @nodes.include?(from) ) || ( not @nodes.keys.include?(to) )
+            node_to_ptr = @nodes[to]
+            node!(from).connect_to(node_to_ptr, edge)            
         end
     end
 
@@ -234,8 +124,27 @@ class Graph
         keywords.each do |keyword|
             new_state = enter!(keyword, new_state)
         end
-
         #compute_aho_corasick_failure()
+    end
+
+    def enter!(keyword, new_state)
+        state = node!(0)
+        keyword_index = 0
+        
+        while state.goto( keyword[keyword_index] )
+            state_id = state.goto( keyword[keyword_index] )
+            state    = node!( state_id )
+            keyword_index += 1
+        end
+
+        keyword_index.upto(keyword.length - 1) { |i|
+            new_state += 1
+            add_node(new_state)
+            add_edge(state.id, new_state, keyword[i])
+            state = node!(new_state)
+        }
+        state.add_output(keyword)
+        return new_state
     end
 
     def find(needle)
@@ -256,31 +165,6 @@ class Graph
         return state.output.include?(needle)
     end
 
-# Functions for Debug Purpose.
-
-    #
-    # For each Node in the Graph, display the value of the output function.
-    #
-    def display_outputs
-        @nodes.values.each do |n|
-            puts "Node #{n.id} --> #{n.output}" if n.output.size > 0
-        end
-    end
-
-    #
-    # For each Node in the Graph, display the neighbors.
-    #
-    def display_neighbors
-        @nodes.values.each do |n|
-            puts "Node #{n.id} --> #{n.neighbors}" if n.neighbors.size > 0
-        end
-    end
-
-    #
-    # Display the entire Graph.
-    # For each node, display the neighbors and the value of the
-    # output function.
-    #
     def display_graph
         @nodes.values.each do |n|
             puts "Node #{n.id}"
@@ -293,25 +177,6 @@ class Graph
     end
 
 private
-    def enter!(keyword, new_state)
-        state = node!(0)
-        keyword_index = 0
-        
-        while state.goto( keyword[keyword_index] )
-            state_id = state.goto( keyword[keyword_index] )
-            state    = node!( state_id )
-            keyword_index += 1
-        end
-
-        keyword_index.upto(keyword.length - 1) { |i|
-            new_state += 1
-            add_node(new_state)
-            add_edge(state.id, new_state, keyword[i])
-            state = node!(new_state)
-        }
-        state.add_output(keyword)
-        return new_state
-    end
 
     def compute_aho_corasick_failure()
         queue = []
--- test/aho-corasick-unittest.rb
+++ test/aho-corasick-unittest.rb
@@ -4,14 +4,146 @@
 #
 # ruby -I src test/aho-corasick-unittest.rb 
 
+require 'simplecov'
+SimpleCov.start
+
 require 'test/unit'
-require 'Text'
+require 'Graph'
 
 
-class TestText < Test::Unit::TestCase
+class TestNode < Test::Unit::TestCase
 
-	def simple_test
-		assert_equal(true, true)
-	end
+    def test_node_constructor
+        # positive tests
+        n = Node.new("Node Name")
+        assert_equal("Node Name", n.name)
+        assert_equal({}, n.edges)
+        assert_equal(0 , n.failure)
+        assert_equal([], n.output)
+        assert_equal(10 , n.failure=10)
+
+        # negative tests
+        n = Node.new()
+        assert_equal("", n.name)
+    end
+
+    def test_connect_to
+        n1 = Node.new("from")
+        n2 = Node.new("to")
+
+        # positive tests
+        assert_not_equal(nil, n1.connect_to(n2,"n1->n2"))
+        assert_equal(    nil, n1.connect_to(n2,"n1->n2"))
+        assert_not_equal(nil, n1.connect_to(n2 , 0))
+
+        # negative tests
+        assert_equal(nil, n1.connect_to(nil, "n1->nil"))
+        assert_equal(nil, n1.connect_to(n2 , nil))
+        assert_equal(nil, n1.connect_to(n2 , ""))
+        assert_equal(nil, n1.connect_to(nil, nil))
+    end
+
+    def test_has_edge?
+        n1 = Node.new("from")
+        n2 = Node.new("to")
+        n1.connect_to(n2, "n1->n2")
+
+        # positive tests
+        assert_equal(true, n1.has_edge?("n1->n2"))
+    
+        # negative tests
+        assert_equal(false, n1.has_edge?("does not exist"))
+    end
+
+    def test_add_output
+        n1 = Node.new("from")
+
+        # positive tests
+        assert_not_equal(nil, n1.add_output("needle1"))
+        assert_equal(["needle1"], n1.output)
+
+        # negative tests
+        assert_equal(nil, n1.add_output(nil))
+        assert_equal(["needle1"], n1.output)
+        
+        assert_equal(nil, n1.add_output(""))
+        assert_equal(["needle1"], n1.output)
+    end
+
+    def test_goto
+        n1 = Node.new("from")
+        n2 = Node.new("to")
+        n1.connect_to(n2, "n1->n2")
+        
+        # positive tests
+        assert_equal(n2, n1.goto("n1->n2"))
+
+        # negative tests
+        assert_equal(nil, n1.goto(nil))
+        assert_equal(nil, n1.goto(""))
+        assert_equal(nil, n1.goto("does not exist"))
+        
+    end
+
+end
+
+class TestGraph < Test::Unit::TestCase
+
+    def test_graph_constructor
+        g = Graph.new()
+        assert_equal({}, g.nodes)
+    end    
+
+    def test_add_node
+        g = Graph.new()
+        
+        # positive tests
+        assert_not_equal(nil, g.add_node("node1"))
+        assert_not_equal(nil, g.add_node(0))
+
+        # negative tests
+        assert_equal(nil, g.add_node(nil))
+        assert_equal(nil, g.add_node(""))
+        assert_equal(nil, g.add_node("node1")) # already inserted
+    end
+
+    def test_node!
+        g = Graph.new()
+        g.add_node("first node")
+        g.add_node("second node")
+
+        # positive tests
+        assert_not_equal(nil, g.node!("first node"))
+        assert_not_equal(nil, g.node!("second node"))
+
+        # negative tests
+        assert_equal(nil, g.node!(""))
+        assert_equal(nil, g.node!(nil))
+    end
+
+    def test_add_edge
+        g = Graph.new()
+        g.add_node("n1")
+        g.add_node("n2")
+
+        # positive tests
+        assert_not_equal(nil, g.add_edge("n1", "n2", "n1->n2"))
+        assert_equal(true, g.nodes["n1"].has_edge?("n1->n2"))
+        
+        # negative tests
+        assert_equal(nil, g.add_edge(nil, "n2", "1->2"))
+        assert_equal(nil, g.add_edge( "", "n2", "1->2"))
+
+        assert_equal(nil, g.add_edge("n1", nil, "1->2"))
+        assert_equal(nil, g.add_edge("n1",  "", "1->2"))
+
+        assert_equal(nil, g.add_edge("n1", "n2", nil))
+        assert_equal(nil, g.add_edge("n1", "n2",  ""))
+
+        assert_equal(nil, g.add_edge("n1", "invalid", "n1->n2"))
+        assert_equal(nil, g.add_edge("invalid", "n1", "n1->n2"))
+
+        assert_equal(false, g.nodes["n1"].has_edge?("n1->does not exist"))
+    end
 
 end
